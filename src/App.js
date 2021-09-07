0f8fe7af2eb6b8e1299a363819f63264d0955904
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-09-07 12:19:32 -0300

    a button was added to hide

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
 import React, { useState } from 'react';
 import image from './images/goku-vegeta.jpg'
 import './App.css';
-
 function App() {
   const [animated, setAnimated] = useState(false)
   const [contador, setContador] = useState(0)
+  const [ocultar, setOcultar] = useState(false)
+
 
   const sumar = () => {
-
     if (!animated) {
       setContador(contador + 1)
     }
   }
-
-
   const animar = () => {
     console.log("click")
     console.log('animated en la funcion :>> ', animated);
     console.log('')
     setAnimated(!animated)
   }
-
   console.log('animated :>> ', animated);
 
+
+  const hide = () => {
+    setOcultar(!ocultar,)
+  }
 
 
   return (
     <div className="App">
       <header className="App-header">
+       
         {
-          animated ? (
+          !ocultar && animated ? (
             <img src='https://i.pinimg.com/originals/0d/a1/e0/0da1e00997d4e3b74ff40d06b7e3cf6a.gif' className="App-logo" alt="Goku y Vegeta" />
-          ) : (
+            ) : !ocultar && !animated ? (
             <img src={image} className="App-logo" alt="Goku y Vegeta" />
-          )
+          ) : ( <div />) 
         }
 
         <p>
-          Pulse boton para animar imagen
+          Animación
         </p>
+
         <div>
           <button className="click" onClick={() => {
             animar()
             sumar()
           }}>
-
             {
               animated ? (
                 <p>Detener</p>
@@ -59,12 +61,25 @@ function App() {
           <div>
             {contador}
           </div>
+          <p>
+            Ocultar imagen
+          </p>
+          <button className="click" onClick={() => {
+            hide()
+          }}>
 
 
+            {
+              ocultar ? (
+                <p>Habilitar</p>
+              ) : (
+                <p>Desabilitar</p>
+              )
+            }
+          </button>
         </div>
       </header>
     </div>
   );
 }
-
 export default App;
